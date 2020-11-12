8b1399089b395d526de91163334b2e4f7eca6285
StefanoSalidu <st.salidu@gmail.com>
2020-11-12 19:15:56 +0100

    style: added console logs to test script

--- test/test.sh
+++ test/test.sh
@@ -4,12 +4,12 @@ cd "$(dirname "$0")"
 
 if [ ! -e immudb ]
 then
-  echo "Downloading immudb..."
-
   if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    echo "Downloading 'linux-gnu' immudb version..."
     URL=https://github.com/vchain-us/immudb/releases/download/v0.8.0/immudb-v0.8.0-linux-amd64
     wget -O immudb $URL
   elif [[ "$OSTYPE" == "darwin"* ]]; then
+    echo "Downloading 'darwin' immudb version..."
     URL=https://github.com/vchain-us/immudb/releases/download/v0.8.0/immudb-v0.8.0-darwin-amd64
     curl -o immudb -L $URL
   fi
