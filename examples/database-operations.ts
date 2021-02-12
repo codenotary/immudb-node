4244b1d37e4af02eb74287fa6c4bddba7d1acea8
StefanoSalidu <st.salidu@gmail.com>
2021-02-12 21:42:37 +0100

    chore: udpated examples

--- examples/database-operations.ts
+++ examples/database-operations.ts
@@ -20,9 +20,8 @@ const IMMUDB_PWD: string = (process.env.IMMUDB_PWD as string || 'immudb');
  
 (async () => {
     try {
-        const rand = '' + Math.floor(Math.random()
-            * Math.floor(100000));
-        let res = null;
+		const randString: string = `${Math.floor(Math.random() * Math.floor(100000))}`;
+		const randNumber: number = +randString;
         let index: any;
 
         // Instantiate the client
@@ -36,169 +35,177 @@ const IMMUDB_PWD: string = (process.env.IMMUDB_PWD as string || 'immudb');
         });
 
         // create database
-        res = await client.createDatabase({
-            databasename: rand
+        await client.createDatabase({
+            databasename: randString
         })
         console.log('success: createDatabase');
 
         // use database just created
-        res = await client.useDatabase({
-            databasename: rand
+        await client.useDatabase({
+            databasename: randString
         })
         console.log('success: useDatabase');
 
         // add new item with the specified key and value
-        res = await client.set({
-            key: rand,
-            value: rand
+        const res1 = await client.set({
+            key: randString,
+            value: randString
         })
-        console.log('success: set', res);
-        index = res && res.index
+        console.log('success: set', res1);
+        // index = res1 && res1.index;
 
         // get item having the specified key
-        res = await client.get({
-            key: rand
+        const res2 = await client.get({
+            key: randString
         })
-        console.log('success: get', res);
+        console.log('success: get', res2);
 
         // count keys having the specified value
-        // in the database in use
-        res = await client.count({ 
-            prefix: rand
-        })
-        console.log('success: count', res);
+        // // in the database in use
+        // const res3 = await client.count({ 
+        //     prefix: randString
+        // })
+        // console.log('success: count', res3);
 
         // increase occurences of items having the
         // same key
         for (let i = 0; i < 10; i++) {
-            res = await client.set({
-                key: rand,
-                value: rand
+            const res4 = await client.set({
+                key: randString,
+                value: randString
             })
-            console.log(`success: set ${i}`, res);
+            console.log(`success: set ${i}`, res4);
         }
             
         // count again keys having the specified value
         // in the database in use (result will be +10)
-        res = await client.count({
-            prefix: rand
-        })
-        console.log('success: count', res);
+        // const res5 = await client.count({
+        //     prefix: randString
+        // })
+        // console.log('success: count', res5);
 
         // iterate over keys having the specified
         // prefix
-        res = await client.scan({
-            prefix: rand,
-            offset: '10',
+        const res6 = await client.scan({
+            seekkey: new Uint8Array(randNumber),
+            prefix: new Uint8Array(randNumber),
+			desc: true,
             limit: 1,
-            reverse: false,
-            deep: false
+			sincetx: randNumber,
+			nowait: true
         })
-        console.log('success: scan', res);
+        console.log('success: scan', res6);
 
         // return an element by index
-        res = await client.byIndex({
-            index: index
-        })
-        console.log('success: byIndex', res);
+        // const res7 = await client.byIndex({
+        //     index: index
+        // })
+        // console.log('success: byIndex', res7);
 
-        // safely return an element by index
-        res = await client.bySafeIndex({
-            index: index
-        })
-        console.log('success: bySafeIndex', res);
+        // // safely return an element by index
+        // const res8 = await client.bySafeIndex({
+        //     index: index
+        // })
+        // console.log('success: bySafeIndex', res8);
 
         // fetch history for the item having the
         // specified key
-        res = await client.history({
-            key: rand,
-            offset: 0,
-            limit: 0,
-            reverse: false
-        })
-        console.log('success: history', res);
+        // const res9 = await client.history({
+		// 	key: new Uint8Array(randNumber),
+		// 	offset: 0,
+		// 	limit: 0,
+		// 	desc: false,
+		// 	sincetx: randNumber
+
+        // })
+        // console.log('success: history', res9);
 
         // fetch paginated history for the item having the
         // specified key
-        res = await client.history({
-            key: rand,
-            offset: 10,
-            limit: 5,
-            reverse: false
-        })
-        console.log('success: paginated history', res);
+        // const res10 = await client.history({
+		// 	key: new Uint8Array(randNumber),
+		// 	offset: 10,
+		// 	limit: 5,
+		// 	desc: true,
+		// 	sincetx: randNumber
+        // })
+        // console.log('success: paginated history', res10);
 
         // iterate over a sorted set
-        res = await client.zScan({
-            set: rand,
-            offset: '10',
-            limit: 5,
-            reverse: false
+        const res11 = await client.zScan({
+			set: randString,
+			seekkey: '',
+			seekscore: 0,
+			seekattx: 0,
+			inclusiveseek: true,
+			limit: 5,
+			desc: true,
+			sincetx: 0,
+			nowait: true
         })
-        console.log('success: zScan', res);
+        console.log('success: zScan', res11);
 
         // iterate over a sorted set
-        res = await client.zAdd({
-            set: rand,
-            key: '10',
-            score: 5,
-            index: 0
-        })
-        console.log('success: zScan', res);
+        // const res12 = await client.zAdd({
+        //     set: randString,
+        //     key: '10',
+        //     score: 5,
+        //     index: 0
+        // })
+        // console.log('success: zScan', res12);
 
         // iterate over all elements by insertion order
-        res = await client.iScan({
-            pagesize: 1,
-            pagenumber: 1
-        })
-        console.log('success: iScan', res);
+        // const res13 = await client.iScan({
+        //     pagesize: 1,
+        //     pagenumber: 1
+        // })
+        // console.log('success: iScan', res13);
 
         // execute a batch read
-        res = await client.getBatch({
-            keysList: [{
-                key: rand
-            }],
+        const res14 = await client.getAll({
+            keysList: [randString],
+			sincetx: randNumber
         })
-        console.log('success: getBatch', res);
+        console.log('success: getAll', res14);
 
         // check immudb health status
-        res = await client.health()
-        console.log('success: health', res);
+        const res15 = await client.health()
+        console.log('success: health', res15);
 
-        // get current root info
-        res = await client.currentRoot()
-        console.log('success: currentRoot', res);
-
-        // safely add new item having the specified key and value
-        res = await client.safeSet({
-            key: rand + 10,
-            value: rand + 10
-        })
-        console.log('success: safeSet', res);
-
-        // get current root info
-        res = await client.currentRoot()
-        console.log('success: currentRoot', res);
+        // get current state
+        const res16 = await client.currentState()
+        console.log('success: currentState', res16);
 
         // safely add new item having the specified key and value
-        res = await client.safeSet({
-            key: rand + 11,
-            value: rand + 11
+        const res17 = await client.verifiedSet({
+            key: randString + 10,
+            value: randString + 10
         })
-        console.log('success: safeSet', res);
+        console.log('success: verifiedSet', res17);
+
+        // get current state
+        const res18 = await client.currentState()
+        console.log('success: currentState', res18);
 
         // safely add new item having the specified key and value
-        res = await client.safeSet({
-            key: rand + 12,
-            value: rand + 12
+        const res19 = await client.verifiedSet({
+            key: randString + 11,
+            value: randString + 11
         })
-        console.log('success: safeSet', res);
+        console.log('success: verifiedSet', res19);
+
+        // safely add new item having the specified key and value
+        const res20 = await client.verifiedSet({
+            key: randString + 12,
+            value: randString + 12
+        })
+        console.log('success: verifiedSet', res20);
 
         // safely get item by key
-        res = await client.safeGet({
-            key: rand + 12
+        const res21 = await client.verifiedGet({
+            key: randString + 12
         })
-        console.log('success: safeGet', res);
+        console.log('success: verifiedGet', res21);
     } catch (err) {
         console.log(err)
     }
